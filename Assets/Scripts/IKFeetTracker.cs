0fce7d420cd13e6b45316964b5b0fa0ccca2aca0
Julian Arnold <julian.arnold@student.hpi.de>
2025-01-11 14:17:28 +0100

    bendable feet

--- Assets/Scripts/IKFeetTracker.cs
+++ Assets/Scripts/IKFeetTracker.cs
@@ -10,6 +10,9 @@ public class IKFeetTracker : MonoBehaviour
     public Transform shinTarget;
     public Transform footTarget;
 
+    public Vector3 shinRotationOffset = new Vector3(-85, 328, -329);
+    public bool shinRotationSign = false;
+
     public bool isAttached = false;
 
     public bool positionReached = false;
@@ -38,8 +41,8 @@ public class IKFeetTracker : MonoBehaviour
             }
         } else {
             if (positionReached) {
-                if (Physics.Raycast(transform.position + Vector3.up * 0.05f, Vector3.down, out RaycastHit hit, 1.0f, ~LayerMask.GetMask("Player"))) {
-                    Attach(hit.transform);
+                if (Physics.Raycast(transform.position + Vector3.up * 0.2f, Vector3.down, out RaycastHit hit, 1.0f, ~LayerMask.GetMask("Player"))) {
+                    Attach(hit.transform, hit.point);
                 }
             }
         }
@@ -50,8 +53,14 @@ public class IKFeetTracker : MonoBehaviour
     void UpdateLinkage() {
         // Simpified version
 
+        float targetDistance = Vector3.Distance(thighTarget.position, transform.position);
+
+        float jointAngle = GetJointAngle(thighLength, shinLength, targetDistance);
+
+        Debug.Log(jointAngle);
+        shinTarget.localRotation = Quaternion.Euler(shinRotationOffset + new Vector3(jointAngle * (shinRotationSign ? -1:1), 0, 0));
+
         targetRotation = Quaternion.FromToRotation(footTarget.position - thighTarget.position, transform.position - thighTarget.position) * thighTarget.rotation;
-        
         
         if (!isAttached) {
             if (Quaternion.Angle(thighTarget.rotation, targetRotation) < 1.0f) positionReached = true;
@@ -66,8 +75,15 @@ public class IKFeetTracker : MonoBehaviour
         //footTarget.rotation = transform.rotation;
     }
 
-    void Attach(Transform t) {
+    float GetJointAngle(float a, float b, float c) {
+        Debug.Log("a: " + a + " b: " + b + " c: " + c);
+        c = Mathf.Clamp(c, 0, a + b - 0.001f);
+        return 180 - Mathf.Rad2Deg * Mathf.Acos((a*a + b*b - c*c) / (2*a*b));
+    }
+
+    void Attach(Transform t, Vector3 location) {
         transform.SetParent(t, true);
+        transform.position = location;
         isAttached = true;
     }
 
