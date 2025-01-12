63182cfddcaccd78ee64afbf8272ee54e36d51d1
Nick <nick@henjes-net.de>
2025-01-12 14:44:58 +0100

    timer and score

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
     public int score;
+    public TMP_Text scoreText;
     // Start is called before the first frame update
     void Start()
     {
         score = 0;
+        scoreText.text = "Score: " + score.ToString();
     }
 
     public void addToScore(int addition) {
         score += addition;
-        Debug.Log(score);
+        // Debug.Log(score);
+        scoreText.text = "Score: " + score.ToString();
     }
 }
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float timeRemaining = 10;
+    private bool timerIsRunning;
+    public float minutes;
+    public float seconds;
+    public TMP_Text timerText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerIsRunning = true;
+    }
+
+    void DisplayTime(float timeToDisplay) {
+        minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        if (seconds >= 10) {
+            timerText.text = minutes.ToString() + ":" + seconds.ToString();
+        }
+        else {
+            timerText.text = minutes.ToString() + ":0" + seconds.ToString();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerIsRunning) {
+            if (timeRemaining > 0) {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+            else {
+                timerIsRunning = false;
+                timeRemaining = 0;
+                DisplayTime(timeRemaining);
+            }
+        }
+    }
+}
