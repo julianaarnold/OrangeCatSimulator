c4f169a16dd411f95ca95f6062c0ca9c105134ea
Nick <nick@henjes-net.de>
2025-01-12 15:17:55 +0100

    scratch and bite score

--- Assets/Scripts/ScoreBehaviour_bite.cs
+++ Assets/Scripts/ScoreBehaviour_bite.cs
@@ -5,15 +5,26 @@ using UnityEngine;
 public class ScoreBehaviour_bite : MonoBehaviour
 {
     public ScoreManager scoreManager;
-    private bool eaten = false;
+    private bool eaten;
+    private int biteCount;
+    public int biteLimit = 2;
 
     // Start is called before the first frame update
     void Start()
     {
         scoreManager = FindObjectOfType<ScoreManager>();
+        biteCount = 0;
+        eaten = false;
     }
 
-    public void scratchScore() {
+    public void biteCounter() {
+        biteCount += 1;
+        if (biteCount == biteLimit) {
+            biteScore();
+        }
+    }
+
+    private void biteScore() {
         if (eaten == false) {
             scoreManager.addToScore(20);
             eaten = true;
